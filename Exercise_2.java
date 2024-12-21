fa34b3f0f1cf418762ddcfa3385515aaafee2fff
Tanish <tanishpargal098765@gmail.com>
2024-12-21 13:43:12 +0530

    PreCourse2 : Exercise_2 completed.

--- Exercise_2.java
+++ Exercise_2.java
@@ -7,12 +7,34 @@ class QuickSort
        pivot and all greater elements to right 
        of pivot */
     void swap(int arr[],int i,int j){
-        //Your code here   
+        //Your code here
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
     
     int partition(int arr[], int low, int high) 
     { 
-   	//Write code here for Partition and Swap 
+   	//Write code here for Partition and Swap
+        //choose any element from the array for pivot.
+        int pivot = arr[high];
+        //index from where we're going to swap the values with.
+        int iIndex = low-1;
+        //loop until we reach to the last index.
+        for(int j = low; j < high; j++){
+            //all the values lower than pivot should be to the left side.
+            if(arr[j] < pivot){
+                //only increment if the current element at j is less than pivot.
+                iIndex++;
+                //swap the values at i and j index.
+                swap(arr,iIndex,j);
+            }
+        }
+        //post the loop is completed, now the current i index will be at last swapped value and pivot is greater than that so we
+        //increment the i index and swap with high as high contains the pivot
+        iIndex++;
+        swap(arr,iIndex,high);
+        return iIndex;
     } 
     /* The main function that implements QuickSort() 
       arr[] --> Array to be sorted, 
@@ -21,7 +43,13 @@ class QuickSort
     void sort(int arr[], int low, int high) 
     {  
             // Recursively sort elements before 
-            // partition and after partition 
+            // partition and after partition
+        if(low < high) {
+            int indexOfPartition = partition(arr, low, high);
+            //sort the element on left side of partition and right side too.
+            sort(arr, low, indexOfPartition - 1);
+            sort(arr, indexOfPartition + 1, high);
+        }
     } 
   
     /* A utility function to print array of size n */
